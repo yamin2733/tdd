d0db104464a109962f37f950edc803d121532e6e
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-15 14:55:03 +0800

    import unittest and updated the functional_tests.py's structure

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,17 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+            self.browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+	def tearDown(self):
+	    self.browser.quit()
+
+	def test_can_start_a_todo_list(self):
+	    self.browser.get("http://localhost:8000")
+	    self.assertIn( "To-Do",self.browser.title) 
+	    self.fail("Finish the test!")
+
+if __name__=="__main__":
+	unittest.main()
