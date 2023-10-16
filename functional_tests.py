2bccefd806a7bf2183919ab50e60403e7ac9563a
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-16 11:28:48 +0800

    First unit test and view function

--- functional_tests.py
+++ functional_tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
-            self.browser = webdriver.Firefox()
+		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
 	    self.browser.quit()
