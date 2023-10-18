9a52d70d440a18ca0028d292b1f76c5f81280c4d
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-18 13:40:19 +0800

    POST works,Save it before refactoring.

--- functional_tests.py
+++ functional_tests.py
@@ -18,15 +18,13 @@ class NewVisitorTest(unittest.TestCase):
 	    self.assertIn("To-Do",header_text)
 	    inputbox = self.browser.find_element(By.ID,"id_new_item")
 	    self.assertEqual(inputbox.get_attribute("placeholder"),"Enter a to-do item")
-	    inputbox.send_keys("Buy peakcock feathers")
+	    inputbox.send_keys("Use peacock feathers to make a fly")
 	    inputbox.send_keys(Keys.ENTER)
-	    time.sleep(1)
+	    time.sleep(5)
 	    table = self.browser.find_element(By.ID,"id_list_table")
 	    rows = table.find_elements(By.TAG_NAME, "tr")
-	    self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
-	    self.fail("Finish the test!")
-
-	    [...]
+	    self.assertIn("Buy peacock feathers",[row.text for row in rows])
+	    self.assertIn("Use peacock feathers to make a fly",[row.text for row in rows])
 
 if __name__=="__main__":
 	unittest.main()
