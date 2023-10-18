8f5c25424b530000c1a7037f9e3620e618fcbf1c
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-18 19:26:18 +0800

    refactor code for method POST two inputs, there is an error to fix

--- functional_tests.py
+++ functional_tests.py
@@ -11,20 +11,28 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 	    self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element(By.ID,"id_list_table")
+		rows = table.find_elements(By.TAG_NAME, "tr")
+		self.assertIn(row_text,[row.text for row in rows])
+
 	def test_can_start_a_todo_list(self):
-	    self.browser.get("http://localhost:8000")
-	    self.assertIn( "To-Do",self.browser.title) 
-	    header_text = self.browser.find_element(By.TAG_NAME,"h1").text
-	    self.assertIn("To-Do",header_text)
-	    inputbox = self.browser.find_element(By.ID,"id_new_item")
-	    self.assertEqual(inputbox.get_attribute("placeholder"),"Enter a to-do item")
-	    inputbox.send_keys("Use peacock feathers to make a fly")
-	    inputbox.send_keys(Keys.ENTER)
-	    time.sleep(5)
-	    table = self.browser.find_element(By.ID,"id_list_table")
-	    rows = table.find_elements(By.TAG_NAME, "tr")
-	    self.assertIn("Buy peacock feathers",[row.text for row in rows])
-	    self.assertIn("Use peacock feathers to make a fly",[row.text for row in rows])
+		self.browser.get("http://localhost:8000")
+
+		inputbox = self.browser.find_element(By.ID,"id_new_item")
+		inputbox.send_keys("Buy peacock feathers")
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
+		self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+		inputbox = self.browser.find_element(By.ID,"id_new_item")
+		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
+
+		self.check_for_row_in_list_table("1: Buy peacock feathers")
+		self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+	    
 
 if __name__=="__main__":
 	unittest.main()
