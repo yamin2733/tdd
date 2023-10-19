25dc17f64f1b9a0ac2973204a0dd7aad54bb45b3
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-19 16:11:10 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,33 @@
 from django.test import TestCase
+from lists.models import Item
 
 # Create your tests here.
 
 class HomgPageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text":"A new list item"})
-        self.assertContains(response,"A new list item")
-        self.assertTemplateUsed(response, "home.html")
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response,"home.html")
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text,"Item the second")
+
+
