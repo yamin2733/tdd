7188b6426972ee3f6728f5ad785984bb67c0eaf4
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-10-27 13:42:48 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -8,10 +8,29 @@ class HomgPageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response,"home.html")
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+      
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"}) 
+        self.assertRedirects(response,"/")
+
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(),0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
