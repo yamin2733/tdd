3a4a12b6a34b7cbfa75eeea1be9bbc6b20c2b72f
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-11-06 15:24:05 +0800

    first steps fo FT for layout + styling

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item,List
 
 # Create your tests here.
 
@@ -9,22 +9,33 @@ class HomgPageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
     
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = mylist
         first_item.save()
         
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = mylist
         second_item.save()
         
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list,mylist)
+
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list,mylist)
         self.assertEqual(second_saved_item.text,"Item the second")
+        self.assertEqual(second_saved_item.list,mylist)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -32,8 +43,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,"list.html")
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        mylist = List.objects.create()
+        Item.objects.create(text="itemey 1",list=mylist)
+        Item.objects.create(text="itemey 2",list=mylist)
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
