28ebb6fd2257878b7fa9d6273558399d028c0637
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-11-23 18:41:46 +0800

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -28,6 +28,25 @@ class ListViewTest(TestCase):
         self.assertContains(response, "itemey 2")
         self.assertNotContains(response, "other list item")
 
+    def test_can_save_a_POST_request_to_an_exisiting_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        self.client.post(
+             f'/lists/{correct_list.id}/', data={'item_text': 'A new item for an existing list'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+   
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/', data={'item_text':'A new item for an existing list'})
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post("/lists/new", data={"item_text": "A new list item"})
@@ -46,7 +65,7 @@ class NewListTest(TestCase):
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
 
-    def test_valisation_erros_are_sent_back_to_home_page_template(self):
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post('/lists/new',data={'item_text':''})
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'home.html')
@@ -58,29 +77,4 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(),0)
         self.assertEqual(Item.objects.count(),0)
 
-class NewItemtTest(TestCase):
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.get()
-        self.assertEqual(new_item.text, "A new item for an existing list")
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
-
-        self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,15 @@ def home_page(request):
    return render(request, "home.html")
 
 def view_list(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    return render(request, "list.html", {"list": our_list})
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+       Item.objects.create(text=request.POST['item_text'],list=list_)
+       return redirect(f'/lists/{list_.id}/')
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'],list=list_)
+    item = Item(text=request.POST['item_text'],list=list_)
     try:
         item.full_clean()
         item.save()
@@ -22,7 +25,4 @@ def new_list(request):
         return render(request,'home.html',{"error":error})
     return redirect(f"/lists/{list_.id}/")
 
-def add_item(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
-    return redirect(f"/lists/{our_list.id}/")
+
