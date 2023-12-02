52e075f4e6839b43da6cd0a41aa531bc7788924f
yamin2733 <61345474+yamin2733@users.noreply.github.com>
2023-12-02 10:21:29 +0800

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -2,9 +2,16 @@ from django.test import TestCase
 from lists.models import Item,List
 from django.core.exceptions import ValidationError
 
+import pdb
+
 # Create your tests here.
 
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
+
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
+
     def test_saving_and_retrieving_items(self):
         mylist = List()
         mylist.save()
@@ -39,6 +46,48 @@ class ListAndItemModelTest(TestCase):
             item.save()
             item.full_clean()
 
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        Item.objects.create(list=list1, text='abc')
+        
+        list2 = List.objects.create()
+        item = Item.objects.create(list=list2, text='abc')
+        
+        item.full_clean()  # should not raise
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
+
+
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
+
+
+class ListModelTest(TestCase):
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
